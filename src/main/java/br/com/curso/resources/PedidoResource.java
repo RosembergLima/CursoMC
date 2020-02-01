a856193906654f8d1c9b4d0128018ac76d31fbc9
Rosemberg Bezerra <rosemberg.bezerra@gmail.com>
2020-02-01 14:49:54 -0300

    Refresh do Token

--- src/main/java/br/com/curso/resources/PedidoResource.java
+++ src/main/java/br/com/curso/resources/PedidoResource.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.com.curso.domain.Categoria;
 import br.com.curso.domain.Pedido;
-import br.com.curso.dto.CategoriaDTO;
 import br.com.curso.services.PedidoService;
 
 @RestController
